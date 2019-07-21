e63f4950e3de53319e530a273f14a20196382b26
Paul Rosero <paul_rosero@yahoo.com>
2019-07-21 15:33:53 +0000

    Automatically backed up by Learn

--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -1,2 +1,9 @@
 class CreateSongs < ActiveRecord::Migration[4.2]
+  def change
+    create_table :songs do |s|
+      s.string :name 
+      s.interger :artist_id 
+      s.interger :genre_id 
+    end
+  end
 end
